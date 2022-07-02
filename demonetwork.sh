a3ad2356550c286aab3420bcadf9122876628a36
farazhashmi21 <developerfaraz@yahoo.com>
2022-07-02 05:21:16 +0000

    GC Command Added. Git remote command removed.

--- demonetwork.sh
+++ demonetwork.sh
@@ -1,8 +1,9 @@
 echo "Project Name: demonetwork";
+echo $(git gc);
 echo $(git init);
 echo $(git add .);
-echo $(git commit -m "first commit");
+echo $(git commit -m "GC Command Added. Git remote command removed.");
 echo $(git branch -M master);
-echo $(git remote add origin https://github.com/farazhashmi21/demonetwork.git);
+#echo $(git remote add origin https://github.com/farazhashmi21/demonetwork.git);
 echo $(git remote -v);
 echo $(git push -u origin master);
